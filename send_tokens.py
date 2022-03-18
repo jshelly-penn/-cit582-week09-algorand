b668694f6ba4a787c028eb3aae9a2cc13491b4c0
j-shelly <jeri.shelly@gmail.com>
2022-03-18 15:36:00 -0400

    python is annoying

--- send_tokens.py
+++ send_tokens.py
@@ -43,7 +43,7 @@ def send_tokens( receiver_pk, tx_amount ):
     # txid = signed_tx.transaction.get_txid()
     
     # try:
-        txid = algod.AlgodClient.send_transaction(signed_tx)
+    txid = algod.AlgodClient.send_transaction(signed_tx)
         # print('Transaction  sent with ID', singed_tx.transaction.get_txid())
         # wait_for_confirmation(algod.AlgodClient, signed_tx.transaction.get_txid())
     # except Exception as e:
